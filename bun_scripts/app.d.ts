3c1906d35de888f1fcba5428a2d5902b06897b72
StaticGold <staticgold@StaticGolds-iMac.local>
2025-03-12 12:02:27 -0700

    Use block timestamp to lessen contract calls, update logs

--- bun_scripts/app.d.ts
+++ bun_scripts/app.d.ts
@@ -1,15 +1,16 @@
-declare module "bun" {
-    interface Env {
-        ENV: string
-        RPC_URL: string
-        NETWORK_PASSPHRASE: string
-        LAUNCHTUBE_URL: string
-        LAUNCHTUBE_JWT: string
-        FARMER_PK: string
-        FARMER_SK: string
-        CONTRACT_ID: string
-        STAKE_AMOUNT: number
-        NONCE_COUNT: number
-        INDEX: number
-    }
+declare module 'bun' {
+  interface Env {
+    ENV: string;
+    RPC_URL: string;
+    NETWORK_PASSPHRASE: string;
+    LAUNCHTUBE_URL: string;
+    LAUNCHTUBE_JWT: string;
+    FARMER_PK: string;
+    FARMER_SK: string;
+    CONTRACT_ID: string;
+    STAKE_AMOUNT: number;
+    NONCE_COUNT: number;
+    INDEX: number;
+    WORK_WAIT_TIME: number;
+  }
 }
